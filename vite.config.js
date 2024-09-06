37b57b9581b995bfa93e64cd9ef23f4868791ad7
Edgar Rivas <webdevedd@gmail.com>
2024-09-06 00:24:32 -0700

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/MES2/',
-  // Other Vite configurations
+  plugins: [vue()],
+  base: '/MES2/', // Adjust according to your deployment
+  resolve: {
+    alias: {
+      'three': 'node_modules/three/build/three.module.js',
+    },
+  },
 });
