b97ac4810e703cc79a9977c91b82ade3a1474b2a
Edgar Rivas <webdevedd@gmail.com>
2024-09-06 00:29:41 -0700

    repaired files

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/MES2/', // Adjust according to your deployment
-  resolve: {
-    alias: {
-      'three': 'node_modules/three/build/three.module.js',
-    },
-  },
+  base: '/MES2/',
 });
